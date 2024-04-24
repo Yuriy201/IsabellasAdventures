7b448cc281703580c0a2ea9d05abd09ef771720a
gmnort1337 <166319819+gmnort1337@users.noreply.github.com>
2024-04-24 20:26:58 +0300

    titlesupdate
    Добавил Титры

---
+++ Isabella's Adventures/Assets/SubtitlesScripts/buttonperexod.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Izmenit_Sceny : MonoBehaviour
+{
+    public string »м€_—цены;
+
+    public void Na_Nazhatie_Knopki()
+    {
+        SceneManager.LoadScene(»м€_—цены);
+    }
+}
