6cea43b9e769a782ceb01305f732efd0743224e7
gmnort1337 <166319819+gmnort1337@users.noreply.github.com>
2024-04-24 21:11:04 +0300

    AddedMainMenu
    Добавил меню и сделал кнопки рабочими

---
+++ Isabella's Adventures/Assets/Scripts/MainMenu/ContinueButtonScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadContinueScene : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("enter");
+    }
+}
