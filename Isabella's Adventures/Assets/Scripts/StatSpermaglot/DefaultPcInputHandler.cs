6de74ee794d12ec4daff01cb5bf043a10c9c1b1e
anon121213 <anon121213@mail.ru>
2024-04-24 21:43:59 +0300

    InputSystem

---
+++ Isabella's Adventures/Assets/Scripts/StatSpermaglot/DefaultPcInputHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class DefaultPcInputHandler: InputHandler
+{
+    private InputSettings _playerInput;
+    
+    public void Awake()
+    {
+        _playerInput = new InputSettings();
+        _playerInput.Enable();
+    }
+
+    private void Update()
+    {
+        if (_playerInput.Gameplay.Jump.IsPressed())
+        {
+            InvokeJumpAction();
+        }
+        
+        GetDirection();
+    }
+    
+    protected override void GetDirection()
+    {
+        Directon = _playerInput.Gameplay.Movement.ReadValue<Vector2>().normalized;
+    }
+}
