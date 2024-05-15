b3c83b5be35529d785248a1e12c711736232abba
anon121213 <anon121213@mail.ru>
2024-05-16 01:15:09 +0300

    coinAnim

--- Assets/Scripts/items/Coin.cs
+++ Assets/Scripts/items/Coin.cs
@@ -1,15 +1,14 @@
-using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
-{
-    [SerializeField] private Transform _topPoint;
-    [SerializeField] private Transform _bottomPoint;
+{ 
+    [SerializeField] private Transform _Point;
     [SerializeField] private float _animTime;
     
     private void Start()
     {
-        StartCoroutine(Animation());
+        transform.DOMove(_Point.position, _animTime).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -17,33 +16,6 @@ public class Coin : MonoBehaviour
         if (other.TryGetComponent<Player>(out Player player))
         {
             Destroy(gameObject);
-        }
-    }
-
-    private IEnumerator Animation()
-    {
-        while (true)
-        {
-            float time = 0;
-            
-            while (time < _animTime)
-            {
-                transform.position = Vector3.Lerp(_topPoint.position, _bottomPoint.position, time / _animTime);
-                time += Time.deltaTime;
-                yield return null;
-            }
-            
-            time = 0;
-            
-            while (time < _animTime)
-            {
-                transform.position = Vector3.Lerp(_bottomPoint.position, _topPoint.position, time / _animTime);
-                time += Time.deltaTime;
-                yield return null;
-            }
-            
-            transform.position = _topPoint.position;
-            yield return null;
         }
     }
 }
