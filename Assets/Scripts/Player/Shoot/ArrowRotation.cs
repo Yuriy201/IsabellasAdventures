c1f1d494558d724dc8eb8444e2abaacaea540280
repack by iluhich <iluhadolmatov@bk.ru>
2024-07-05 14:08:32 +0300

    arrow arc test

---
+++ Assets/Scripts/Player/Shoot/ArrowRotation.cs
@@ -0,0 +1,40 @@
+using CustomAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ArrowRotation : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+
+    [ReadOnlyProperty]
+    [SerializeField]
+    private bool canRotate = true;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        canRotate = true;
+    }
+
+    private void Update()
+    {
+        if (canRotate)
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, Mathf.Atan2(rigidbody.velocity.y, rigidbody.velocity.x) * Mathf.Rad2Deg);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canRotate = false;
+    }
+
+    private void OnDisable()
+    {
+        canRotate = false;
+    }
+}
