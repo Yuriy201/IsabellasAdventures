48b134290ad2a4c002a7062db3ea80027b3b289a
repack by iluhich <112997166+gigachad1488@users.noreply.github.com>
2024-07-10 20:12:23 +0300

    moving platform (half ready)
    todo:
    easing,
    a little bit of refactoring

---
+++ Assets/Editor/MovingPlatformEditor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MovingPlatform))]
+public class MovingPlatformEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        MovingPlatform platform = (MovingPlatform)target;
+
+        if (GUILayout.Button("Reset points positions"))
+        {
+            for (int i = 0; i < platform._movePoints.Length; i++)
+            {
+                platform._movePoints[i] = platform.transform.position;
+            }
+        }
+    }
+}
