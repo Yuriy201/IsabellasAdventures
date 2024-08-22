b875656bad5c265913369bb433836e47a3795deb
LikoDen <densorlov94@gmail.com>
2024-08-22 19:36:31 +0300

    1/2
    -Закинул зелья
    -Стало больше врагов( в конце уровня)
    исправлен баг со стрелами
    -Прохождение уровня занимает больше 15 минут при уничтожении всех врагов
    -добавлена механика платформы вверх и вниз

--- Assets/Scripts/Player/Shoot/Bullet.cs
+++ Assets/Scripts/Player/Shoot/Bullet.cs
@@ -2,6 +2,7 @@ using Enemy;
 using NeoxiderAudio;
 using System;
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
@@ -13,10 +14,21 @@ public class Bullet : MonoBehaviour
     private Rigidbody2D _rigidbody;
 
     public event Action<Collider2D>? OnHit;
-    
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+      
+           
+            _rigidbody.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        
+
+    }
+
+    private void Start()
+    {
+       
+        StartCoroutine(DestroyAfterLifetime());
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -25,16 +37,23 @@ public class Bullet : MonoBehaviour
 
         if (collision.TryGetComponent(out IDamagable target))
         {
-            OnHit.Invoke(collision);
+            OnHit?.Invoke(collision);
             target.GetDamage(_damage);
             AudioManager.PlaySound(ClipType.arrowHit);
         }
 
-        //ObjectPool.Instance.ReternObject(gameObject);
+        // Return to pool if using object pooling
+        // ObjectPool.Instance.ReternObject(gameObject);
     }
 
     public void ApplyVelocity()
     {
         _rigidbody.velocity = transform.right * _speed;
+    }
+
+    private IEnumerator DestroyAfterLifetime()
+    {
+        yield return new WaitForSeconds(_lifeTime);
+        Destroy(gameObject);
     }
 }
